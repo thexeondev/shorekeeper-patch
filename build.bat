9c9254a55029e4a8a7a196e1e3ab16d723afb29b
xavo95 <xavo95@xeondev.com>
2024-10-01 19:55:18 +0200

    Launcher changes incoming

--- build.bat
+++ build.bat
@@ -1,6 +1,23 @@
+GOTO:MAIN
+
+:cargoReleaseBuild
+    SETLOCAL ENABLEDELAYEDEXPANSION
+        cargo clean
+        cargo build --release --no-default-features -F %~1
+        set features=%~1:,=-%
+        COPY target\release\shorekeeper.dll build\shorekeeper-%~1.dll
+    ENDLOCAL
+EXIT /B 0
+
+:MAIN
+if exist "build" rd /q /s "build"
+mkdir build
+
 : Build for cn_beta_1_3_0
-cargo build --release --no-default-features -F cn_beta_1_3_0
+call:cargoReleaseBuild cn_beta_1_3_0
 : Build for cn_live_1_3_0
-cargo build --release --no-default-features -F cn_live_1_3_0
+call:cargoReleaseBuild cn_live_1_3_0
 : Build for os_live_1_3_0
-cargo build --release --no-default-features -F os_live_1_3_0
+call:cargoReleaseBuild os_live_1_3_0
+
+tar -acvf shorekeeper-patch-win64.zip -C build
